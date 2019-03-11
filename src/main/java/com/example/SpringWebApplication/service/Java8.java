d159bb4d474aa4d338bf4465f469a24dddceb0b7
rakesh kapri <rakeshkapri@gmail.com>
2019-03-11 21:04:37 +0000

    fabonacci series using supplier, stream.generate

--- src/main/java/com/example/SpringWebApplication/service/Java8.java
+++ src/main/java/com/example/SpringWebApplication/service/Java8.java
@@ -7,4 +7,15 @@ public class Java8 {
         fabonacciUsingSupplier();
     }
 
+    private static void fabonacciUsingSupplier(){
+        int[] i  = {0,1};
+        Stream<Integer> fabonacciSeries = Stream.generate(() -> {
+            int result = i[1];
+            int temp = i[1];
+            i[1] = i[0] + i[1];
+            i[0] = temp;
+            return result;
+        });
+        fabonacciSeries.limit(5).forEach( num -> System.out.println(num));
+    }
 }
