c4437afe771040053c134d8c563f9e01d6257b42
rakesh kapri <rakeshkapri@gmail.com>
2019-03-11 21:12:02 +0000

    Fabonacci series using stream iterate function

--- src/main/java/com/example/SpringWebApplication/service/Java8.java
+++ src/main/java/com/example/SpringWebApplication/service/Java8.java
@@ -1,10 +1,12 @@
 package com.example.SpringWebApplication.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Java8 {
     public static void main(String s[]){
-        fabonacciUsingSupplier();
+        fabonacciUsingIterator();
     }
 
     private static void fabonacciUsingSupplier(){
@@ -17,5 +19,14 @@ public class Java8 {
             return result;
         });
         fabonacciSeries.limit(5).forEach( num -> System.out.println(num));
+    }
+
+    private static void fabonacciUsingIterator(){
+        int limit = 10;
+        List<Integer> list = Stream.iterate(new int[]{0,1}, i -> new int[]{i[1], i[0] + i[1]})
+        .limit(limit)
+        .map(i-> i[0])
+        .collect(Collectors.toList());
+        list.forEach(System.out :: println);
     }
 }
